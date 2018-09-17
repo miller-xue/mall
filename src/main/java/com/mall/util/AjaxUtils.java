01246ecf2424370eaee37c5dbc53a3ebe673ac13
miller_xue <1021362065@qq.com>
2018-09-17 21:33:33 +0800

    project init commit

---
+++ src/main/java/com/mall/util/AjaxUtils.java
@@ -0,0 +1,21 @@
+package com.mall.util;
+
+import org.springframework.web.context.request.WebRequest;
+
+/**
+ * Created by miller on 2018/9/17
+ * @author Miller
+ */
+public class AjaxUtils {
+
+    public static boolean isAjaxRequest(WebRequest webRequest) {
+        String requestedWith = webRequest.getHeader("X-Requested-With");
+        return requestedWith != null ? "XMLHttpRequest".equals(requestedWith) : false;
+    }
+
+    public static boolean isAjaxUploadRequest(WebRequest webRequest) {
+        return webRequest.getParameter("ajaxUpload") != null;
+    }
+
+    private AjaxUtils() {}
+}
